34a838ecc9b78d102fe1995183fb27b7631a67e7
chintan <pawarchintan786@gmail.com>
2023-02-10 14:48:22 +0530

    ci-platform mvc solution  uploaded

---
+++ CI Project/CI-MVC/CIPlatform/CIPlatformweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CIPlatformweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
