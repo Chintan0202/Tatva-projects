f0883a55cbdfdff46c2d64b4aa0c5a9efd65d4ee
chintan <pawarchintan786@gmail.com>
2023-02-24 18:21:43 +0530

    storyListing Page Done

--- CI Project/CI-MVC/CIPlatform/CIPlatformweb/Controllers/HomeController.cs
+++ CI Project/CI-MVC/CIPlatform/CIPlatformweb/Controllers/HomeController.cs
@@ -42,6 +42,10 @@ namespace CIPlatformweb.Controllers
         {
             return View();
         }
+        public IActionResult StoryPage()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
